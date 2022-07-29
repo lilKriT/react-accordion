cf5b0c3d7cd4da00bcd588986e8db23f389b218e
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-29 22:16:15 +0200

    Displaying questions and answers.

--- src/SingleQuestion.js
+++ src/SingleQuestion.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const SingleQuestion = () => {
-  return <div>SingleQuestion</div>;
+const SingleQuestion = ({ title, info }) => {
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn">Button</button>
+      </header>
+      <p>{info}</p>
+    </article>
+  );
 };
 
 export default SingleQuestion;
